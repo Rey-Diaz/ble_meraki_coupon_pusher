1d46e9b66989a909a2267c7dd464432c79e5bc78
Rey-Diaz <Reinoldiaz@gmail.com>
2023-12-20 13:01:32 -0500

    working notifications

--- frontend/src/components/pages/notificationpage.jsx
+++ frontend/src/components/pages/notificationpage.jsx
@@ -1,71 +1,108 @@
-import { useEffect, useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Dialog, DialogTitle, DialogContent, Typography } from '@mui/material';
+import { Component } from 'react';
+import { Typography, Paper, List, ListItem, ListItemText, Snackbar, Alert } from '@mui/material';
 
-function NotificationPage() {
-  const [data, setData] = useState([]);
-  const [openPopup, setOpenPopup] = useState(false);
-  const [popupData, setPopupData] = useState(null);
-
-  useEffect(() => {
-    const eventSource = new EventSource('http://localhost:8000/events');
-    eventSource.onmessage = (event) => {
-      const newData = JSON.parse(event.data);
-      setData(newData);
-      checkForLowRSSI(newData);
+class MerakiDashboardComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      notifications: [],
+      showNotification: false,
+      currentNotification: null,
+      notificationSentForDevice: {}, // Track devices for which notifications have been sent
     };
-    return () => {
-      eventSource.close();
-    };
-  }, []);
+  }
 
-  const checkForLowRSSI = (data) => {
-    const lowRSSIDevice = data.find(device => device.rssi > -40);
-    if (lowRSSIDevice) {
-      setPopupData(lowRSSIDevice);
-      setOpenPopup(true);
+  componentDidMount() {
+    this.startListeningForEvents();
+  }
+
+  componentWillUnmount() {
+    this.stopListeningForEvents();
+  }
+
+  startListeningForEvents = () => {
+    this.eventSource = new EventSource('http://localhost:8000/events');
+    this.eventSource.onmessage = (event) => {
+      try {
+        const newData = JSON.parse(event.data);
+        this.setState({ data: newData }, () => {
+          this.checkForNotifications(newData);
+        });
+      } catch (error) {
+        console.error('Error parsing SSE data:', error);
+      }
+    };
+    this.eventSource.onerror = (error) => {
+      console.error('SSE Error:', error);
+    };
+  };
+
+  stopListeningForEvents = () => {
+    if (this.eventSource) {
+      this.eventSource.close();
     }
   };
 
-  return (
-    <div>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>#</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>UUID</TableCell>
-              <TableCell>Major</TableCell>
-              <TableCell>Minor</TableCell>
-              <TableCell>RSSI</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
+  checkForNotifications = (newData) => {
+    const { notificationSentForDevice } = this.state;
+
+    newData.forEach((device) => {
+      if (device.nearest_ap_rssi > -40 && !notificationSentForDevice[device.id]) {
+        // Check if RSSI is greater than -40 and a notification hasn't been sent for this device
+        this.setState((prevState) => ({
+          notifications: [...prevState.notifications, device],
+          showNotification: true,
+          currentNotification: device,
+          notificationSentForDevice: {
+            ...prevState.notificationSentForDevice,
+            [device.id]: true, // Mark the device as notified
+          },
+        }));
+      }
+    });
+  };
+
+  closeNotification = () => {
+    this.setState({ showNotification: false, currentNotification: null });
+  };
+
+  render() {
+    const { data, notifications, showNotification, currentNotification } = this.state;
+
+    return (
+      <div>
+        <Typography variant="h5" style={{ margin: '20px 0' }}>
+          Meraki Dashboard
+        </Typography>
+        <Paper style={{ margin: '20px 0', padding: '20px' }}>
+          <Typography variant="h6">Condensed Device List</Typography>
+          <List>
             {data.map((device, index) => (
-              <TableRow key={index}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>{device.name}</TableCell>
-                <TableCell>{device.uuid}</TableCell>
-                <TableCell>{device.major}</TableCell>
-                <TableCell>{device.minor}</TableCell>
-                <TableCell>{device.rssi}</TableCell>
-              </TableRow>
+              <ListItem key={index}>
+                <ListItemText primary={device.name || 'N/A'} secondary={`RSSI: ${device.nearest_ap_rssi}`} />
+              </ListItem>
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      {openPopup && (
-        <Dialog open={openPopup} onClose={() => setOpenPopup(false)}>
-          <DialogTitle>Device Details</DialogTitle>
-          <DialogContent>
-            {popupData && Object.entries(popupData).map(([key, value]) => (
-              <Typography key={key}>{`${key}: ${value}`}</Typography>
+          </List>
+        </Paper>
+        <Paper style={{ margin: '20px 0', padding: '20px' }}>
+          <Typography variant="h6">Notifications</Typography>
+          <List>
+            {notifications.map((notification, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={`Device: ${notification.name}`} secondary={`RSSI: ${notification.nearest_ap_rssi}`} />
+              </ListItem>
             ))}
-          </DialogContent>
-        </Dialog>
-      )}
-    </div>
-  );
+          </List>
+        </Paper>
+        <Snackbar open={showNotification} autoHideDuration={6000} onClose={this.closeNotification}>
+          <Alert onClose={this.closeNotification} severity="info" sx={{ width: '100%' }}>
+            {currentNotification ? `Notification for ${currentNotification.name}: RSSI ${currentNotification.nearest_ap_rssi}` : ''}
+          </Alert>
+        </Snackbar>
+      </div>
+    );
+  }
 }
 
-export default NotificationPage;
+export default MerakiDashboardComponent;
